8e1d92e5127a9bee52ca556e32e1013b6964ce08
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2023-07-01 09:52:20 +0800

    :sparkles: update android home work

--- src/test/java/com/odde/atddv2/AndroidSteps.java
+++ src/test/java/com/odde/atddv2/AndroidSteps.java
@@ -5,18 +5,23 @@ import io.appium.java_client.android.AndroidElement;
 import io.cucumber.java.en.Given;
 import lombok.SneakyThrows;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class AndroidSteps {
+
+    @Value("${appium.udid:emulator-5558}")
+    private String udid;
+
     @SneakyThrows
     @Given("input username {string}")
     public void inputUserName(String name) {
         AndroidDriver<AndroidElement> driver;
         DesiredCapabilities caps = DesiredCapabilities.android();
         caps.setCapability("automationName", "UiAutomator2");
-        caps.setCapability("udid", "emulator-5558");
+        caps.setCapability("udid", udid);
         caps.setCapability("platformVersion", "11.0");
         caps.setCapability("platformName", "Android");
         caps.setCapability("app", "/tmp/app.apk");
